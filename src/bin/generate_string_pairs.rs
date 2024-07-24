8ce54d7c6920307cb6ca66c2b546e8673d6af607
Zach Keyes <56801478+imzoc@users.noreply.github.com>
2024-07-24 10:34:19 +0200

    beginning transition to fasta input files

--- src/bin/generate_string_pairs.rs
+++ src/bin/generate_string_pairs.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
-use std::io::{Write, Result};
-
+use std::io::{self, Result, Write};
+use bio::io::fasta;
 use alignment_free_methods::utils::sequence::generate_pair;
 
 /*
@@ -11,19 +11,34 @@ fn main() -> Result<()> {
     let n_sequences = 5;
     let sequence_length = 1000;
     let starting_edit_distance = 0;
-    let max_edit_distance = (0.4 * sequence_length as f64) as usize;
+    let max_edit_distance = (0.6 * sequence_length as f64) as usize;
     let edit_distance_range = starting_edit_distance..max_edit_distance;
 
-    let mut file = File::create(format!("tests/inputs/sequences_{:?}.csv", sequence_length))?;
-    writeln!(&mut file, "base_sequence,modified_sequence,edit_distance")?;
+    let path = "tests/artificial.fasta";
+    let file = File::create(path)?;
+    let mut writer = fasta::Writer::new(file);
 
+    let mut i = 0;
     for _ in 0..n_sequences {
         for edit_distance in edit_distance_range.clone() {
             let (base_sequence_chars, modified_sequence_chars) = generate_pair(sequence_length, edit_distance);
 
             let base_string: String = base_sequence_chars.iter().collect();
             let modified_string: String = modified_sequence_chars.iter().collect();
-            writeln!(&mut file, "{:?},{:?},{}", base_string, modified_string, edit_distance)?;
+            i += 1;
+            writer.write_record(&fasta::Record::with_attrs(
+                &format!("FAKE_{}", i), 
+                Some(&format!("Artificially generated seed. {} edits away from FAKE_{}", edit_distance, i + 1)),
+                base_string.as_bytes())
+            )?;
+            i += 1;
+            writer.write_record(&fasta::Record::with_attrs(
+                &format!("FAKE_{}", i), 
+                Some(&format!("Artificially generated seed. {} edits away from FAKE {}", edit_distance, i - 1)),
+                modified_string.as_bytes())
+            )?;
+            print!("\rString pair #{:?} has been processed!", i);
+            io::stdout().flush()?;
         }
     }
 
