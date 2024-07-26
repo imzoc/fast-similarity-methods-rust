fba8c5f56d05c32d0b186ce2122b0bdf6f942451
Zach Keyes <56801478+imzoc@users.noreply.github.com>
2024-07-26 15:53:16 +0200

    removed seeds database. Can reimplement from previous commit if I want to store seeds again

--- src/bin/strobemer_comparison.rs
+++ src/bin/strobemer_comparison.rs
@@ -61,44 +61,6 @@ fn initialize_comparison_db(filename: PathBuf) -> Result<Connection> {
     Ok(conn)
 }
 
-fn initialize_seeds_db(filename: PathBuf) -> Result<Connection> {
-    let conn = Connection::open(filename)?;
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS seeds (
-            id INTEGER PRIMARY KEY,
-            seq_name TEXT NOT NULL,
-            seed_name TEXT NOT NULL,
-            seed TEXT NOT NULL,
-            time TEXT NOT NULL
-        )",
-        [],
-    )?;
-    Ok(conn)
-}
-fn fetch_seeds(conn: &Connection, seq_name: &str, seed_name: &str) -> Result<Vec<Vec<char>>> {
-    let mut seeds = Vec::new();
-    let mut stmt = conn.prepare(
-        "SELECT seed FROM seeds WHERE seq_name = ?1 AND seed_name = ?2")?;
-    let mut rows = stmt.query(params![seq_name, seed_name])?;
-
-    while let Some(row) = rows.next()? {
-        let value: String = row.get(0)?; // Assuming the column type is TEXT
-        seeds.push(value.chars().collect());
-    }
-    Ok(seeds)
-}
-
-fn store_seeds(conn: &Connection, seeds: &Vec<Vec<char>>, seq_name: &str, seed_name: &str) -> Result<()> {
-    for seed in seeds {
-        conn.execute(
-            "INSERT INTO seeds (seq_name, seed_name, seed, time) VALUES (?1, ?2, ?3, ?4)",
-            params![seq_name, seed_name, String::from_iter(seed), 0],
-        )?;
-    }
-    
-    Ok(())
-}
-
 // --------------------------------------------------
 // See this repo's README file for pseudocode
 fn run(args: StrobemerArgs) -> Result<()> {
@@ -113,42 +75,24 @@ fn run(args: StrobemerArgs) -> Result<()> {
     let comparison_db_conn = initialize_comparison_db(
         Path::new(&project_dir).join("tests/outputs/comparisons.db")
     )?;
-    let seeds_db_conn = initialize_seeds_db(
-        Path::new(&project_dir).join("tests/outputs/seeds.db")
-    )?;
 
     let query_reader = fasta::Reader::from_file(
         Path::new(&project_dir)
             .join("tests/inputs")
             .join(&args.common.query_file))?;
 
-    let mut seeds_generated = 0;
-    let mut seeds_retrieved = 0;
     let mut i = 0;
     for query_record in query_reader.records() {
         let query_record = query_record?;
-        let query_seeds: Vec<Vec<char>> = match seeds_db_conn.prepare(
-            "SELECT 1 FROM seeds WHERE seq_name = ?1 AND seed_name = ?2")?
-            .exists(params![query_record.id(), seed_name])? {
-            true => {
-                seeds_retrieved += 1;
-                fetch_seeds(&seeds_db_conn, query_record.id(), &seed_name)?
-            },
-            false => {
-                let seeds = alignment_free_methods::generate_strobemers(
-                    query_record.seq(),
-                    args.order.clone(),
-                    args.strobe_length.clone(),
-                    args.strobe_window_gap.clone(),
-                    args.strobe_window_length.clone(),
-                    args.common.step.clone(),
-                    None
-                )?;
-                store_seeds(&seeds_db_conn, &seeds, query_record.id(), &seed_name)?;
-                seeds_generated += 1;
-                seeds
-            }
-        };
+        let query_seeds = alignment_free_methods::generate_strobemers(
+            query_record.seq(),
+            args.order.clone(),
+            args.strobe_length.clone(),
+            args.strobe_window_gap.clone(),
+            args.strobe_window_length.clone(),
+            args.common.step.clone(),
+            None
+        )?;
         let reference_reader = fasta::Reader::from_file(
             Path::new(&project_dir)
                 .join("tests/inputs")
@@ -160,28 +104,17 @@ fn run(args: StrobemerArgs) -> Result<()> {
             //let reference_seeds: Vec<Vec<char>> = match seeds_db_conn.prepare(
             //    "SELECT seed FROM seeds WHERE seq_name = ?1 AND seed_name = ?2")?
             //    .exists(params![reference_record.id(), seed_name])? {
-            let reference_seeds: Vec<Vec<char>> = match false {
-                true => {
-                    seeds_retrieved += 1;
-                    fetch_seeds(&seeds_db_conn, reference_record.id(), &seed_name)?
-                },
-                false => {
-                    let seeds = alignment_free_methods::generate_strobemers(
-                        reference_record.seq(),
-                        args.order.clone(),
-                        args.strobe_length.clone(),
-                        args.strobe_window_gap.clone(),
-                        args.strobe_window_length.clone(),
-                        args.common.step.clone(),
-                        None
-                    )?;
-                    // store_seeds(&seeds_db_conn, &seeds, reference_record.id(), &seed_name)?;
-                    seeds_generated += 1;
-                    seeds
-                }
-            };
+            let reference_seeds: Vec<Vec<char>> = alignment_free_methods::generate_strobemers(
+                reference_record.seq(),
+                args.order.clone(),
+                args.strobe_length.clone(),
+                args.strobe_window_gap.clone(),
+                args.strobe_window_length.clone(),
+                args.common.step.clone(),
+                None
+            )?;
 
-            print!("\rseeds generated:{:?} | seeds retrieved:{:?}", seeds_generated, seeds_retrieved);
+            print!("\rseeds generated:{:?}", i);
             io::stdout().flush().unwrap();
             
             let start = Instant::now();
